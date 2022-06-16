b4afbe0a72df69807027acc84c1a836cc142d5ed
Nik0di3m <norbert.waleszczyk@icloud.com>
2022-06-16 02:21:42 +0200

    redux persist starter

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,15 @@
 import '../styles/globals.css'
-
+import { persistor, store } from '../store/store'
+import { PersistGate } from 'redux-persist/integration/react'
+import { Provider } from 'react-redux'
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+    return (
+        <Provider store={store}>
+            <PersistGate loading={null} persistor={persistor}>
+                <Component {...pageProps} />
+            </PersistGate>
+        </Provider>
+    )
 }
 
 export default MyApp
